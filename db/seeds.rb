101119b8f2b5e381914672163b17ec028c2f1ada
Arthur Philadelpho <arthur22rj@gmail.com>
2018-08-07 17:24:23 -0300

    Day 27: Rails CRUD - Rails Task Manager - complete.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Task.create title: 'Hustle hard', details: 'count and stack money'
+Task.create title: 'Buy Goyard Bag', details: 'spend a nickle for a Goyard laptop shoulder bag'
+Task.create title: 'Buy Gucci slippers', details: 'go to the Gucci store at barra and spend a dub'
+Task.create title: 'Buy Versace robe', details: 'go to Versace at Village Mall and spend a dime', completed: true
